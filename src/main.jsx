7b7493767d824e46ed0877362474909b85579dea
Iqrash122 <xagent891@gmail.com>
2025-11-12 14:36:10 +0500

    'iqrash'

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './dashboard.jsx'
+import App from './pages/dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 
 function Dashboard() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
