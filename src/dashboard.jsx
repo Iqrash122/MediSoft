52bcde19738fb8967068db2109c1dfb1c0d2093a
Iqrash122 <xagent891@gmail.com>
2025-11-12 14:34:54 +0500

    'iqrash'

---
+++ src/dashboard.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function Dashboard() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default Dashboard;
